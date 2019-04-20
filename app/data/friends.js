d6e8594237bbdb70f3a375ce64ab00bfd7752fc6
Jessica Elias <jm.elias1990@yahoo.com>
2019-04-20 11:51:04 -0400

    friend-finder

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [ {
+    "name" : "Jonathan",
+    "photo" : "images/guy1.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Crystal",
+    "photo" : "images/girl1.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Brandon",
+    "photo" : "images/guy2.jpg",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Brenda",
+    "photo" : "images/girl2.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Corey",
+    "photo" : "images/guy3.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Jessie",
+    "photo" : "images/girl3.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "DJ",
+    "photo" : "images/guy4.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Sandra",
+    "photo" : "images/girl4.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Andy",
+    "photo" : "images/guy5.jpg",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Karren",
+    "photo" : "images/girl5.jpg",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Mark",
+    "photo" : "images/guy6.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Crissy",
+    "photo" : "images/girl6.jpg",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Chad",
+    "photo" : "images/guy7.jpg",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Becca",
+    "photo" : "images/girl7.jpg",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
